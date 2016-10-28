b6f2a50cccd2bf9df3a441c6846b236bad548c2a
mohan <banmohan@outlook.com>
2016-10-28 12:34:39 +0545

    Api implemented on customer dropdown in sales

--- Controllers/Backend/Setups/CustomerController.cs
+++ Controllers/Backend/Setups/CustomerController.cs
@@ -1,5 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using Frapid.Dashboard;
+using Frapid.WebsiteBuilder.ViewModels;
+using MixERP.Sales.ViewModels;
+using SearchResult = MixERP.Sales.ViewModels.SearchResult;
 
 namespace MixERP.Sales.Controllers.Backend.Setups
 {
@@ -10,6 +17,21 @@ namespace MixERP.Sales.Controllers.Backend.Setups
         public ActionResult Index()
         {
             return this.FrapidView(this.GetRazorView<AreaRegistration>("Setup/Customers.cshtml", this.Tenant));
+        }
+
+        [Route("dashboard/sales/setup/customer-search/{query}")]
+        public async Task<ActionResult> SearchCustomer(string query)
+        {
+            try
+            {
+                List<SearchResult> result = await DAL.Backend.Service.SearchCustomer.SearchCustomers(this.Tenant, query.Replace("\\","").Trim()).ConfigureAwait(false);
+
+                return this.Ok(result);
+            }
+            catch (System.Exception ex)
+            {
+                return this.Failed(ex.Message, HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
---
+++ DAL/Backend/Service/SearchCustomer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Frapid.DataAccess;
+using Frapid.WebsiteBuilder.ViewModels;
+using SearchResult = MixERP.Sales.ViewModels.SearchResult;
+
+namespace MixERP.Sales.DAL.Backend.Service
+{
+    public static class SearchCustomer
+    {
+        public static async Task<List<ViewModels.SearchResult>> SearchCustomers(string tenant, string query)
+        {
+            const string sql = @"SELECT customer_name ""name"", customer_id ""value"" FROM inventory.customers
+                WHERE UPPER(inventory.customers.customer_name)LIKE @0
+                OR UPPER(inventory.customers.contact_address_line_1) LIKE @0
+                OR UPPER(inventory.customers.contact_address_line_2) LIKE @0
+                OR UPPER(inventory.customers.contact_street) LIKE @0
+                OR UPPER(inventory.customers.contact_city) LIKE @0
+                OR UPPER(inventory.customers.contact_phone_numbers) LIKE @0
+                ORDER BY inventory.customers.customer_id
+                LIMIT 5;";
+
+            var result = (List<SearchResult>) await Factory.GetAsync<ViewModels.SearchResult>(tenant, sql, "%" + query.ToUpper() + "%").ConfigureAwait(false);
+            return result;
+        }
+    }
+}
